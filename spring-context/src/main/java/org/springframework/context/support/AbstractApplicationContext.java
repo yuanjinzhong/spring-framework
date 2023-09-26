b5e876c34cd844389093732d112f7476b33e27f1
jinzhong.yuan <jinzhong.yuan@huolala.cn>
2023-09-26 15:29:47 +0800

    事件监听+1

--- spring-context/src/main/java/org/springframework/context/support/AbstractApplicationContext.java
+++ spring-context/src/main/java/org/springframework/context/support/AbstractApplicationContext.java
@@ -907,6 +907,12 @@ public abstract class AbstractApplicationContext extends DefaultResourceLoader
 
 		/**
 		 * 这时候有beandefinition吗？能取到吗？
+		 *
+		 * 结论是可以取到，参考dubbo的各种ApplicaitonEventListener,在这里已经可以取到，
+		 *
+		 * beandefinition在BPP的处理中，已经加入bean工厂
+		 *
+		 * 所以这里表示 【将用户的实现的ApplicationListener添加到多播器里面】
 		 */
 		// Do not initialize FactoryBeans here: We need to leave all regular beans
 		// uninitialized to let post-processors apply to them!
@@ -916,7 +922,11 @@ public abstract class AbstractApplicationContext extends DefaultResourceLoader
 		}
 
 		/**
-		 * 只是发送早期的event,用户的消息 永远是publish为起点
+		 * 只是发送早期的event（系统自身的消息）
+		 *
+		 * 不发送用户消息，
+		 *
+		 * 用户的消息 永远是publish为起点（用户的消息得手动调用ApplicationContext的push方法才会触发）
 		 */
 		// Publish early application events now that we finally have a multicaster...
 		Set<ApplicationEvent> earlyEventsToProcess = this.earlyApplicationEvents;
